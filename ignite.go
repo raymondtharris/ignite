f05b2b29a77311e8095cb6a128dbfa32c2ada664
Tim Harris <raymondtimothyharris@gmail.com>
2015-10-28 23:03:10 -0700

    Prompting
    Starting the section after introducing the user to magna.

--- ignite.go
+++ ignite.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+  "magna/magnaio"
+  "magna/magnauser"
 )
+
+var theUser magnauser.User
 
 func intro() {
 	fmt.Println("Who awakens me?")
+  dataString := magnaio.ReadInput()
+  //fmt.Println(dataString)
+  theUser = magnauser.User{dataString, 12290323094}
+  fmt.Print("Hello "+ theUser.Name + ". ")
+}
 
+func prompt(){
+  fmt.Print("What would you like to know?\n")
+  promptString := magnaio.ReadInput()
+  fmt.Println(promptString)
 }
 
 func main() {
 	intro()
+  prompt()
 }
