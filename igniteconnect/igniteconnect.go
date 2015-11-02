2705921dee31524dd27b8dc4ec4cb7b74ad8e7f6
Tim Harris <raymondtimothyharris@gmail.com>
2015-11-01 20:59:37 -0800

    Sending requests
    Started the code to send requests to Magna for them to be fulfilled.

--- igniteconnect/igniteconnect.go
+++ igniteconnect/igniteconnect.go
@@ -1,6 +1,7 @@
 package igniteconnect
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 
@@ -14,6 +15,7 @@ var upgrader = websocket.Upgrader{
 
 var connection *websocket.Conn
 
+//ConnectToMagna function sets up a socket for magna to return data to the user.
 func ConnectToMagna() {
 	var res *http.Response
 	var err error
@@ -22,12 +24,31 @@ func ConnectToMagna() {
 	connection, res, err = websocket.NewClient(conn, req.URL, req.Header, 1024, 1024)
 	_ = res
 	_ = err
+	defer connection.Close()
+	for {
+		messageType, p, err1 := connection.ReadMessage()
+		if err1 != nil {
+			return
+		}
+		fmt.Println(string(p))
+		err1 = connection.WriteMessage(messageType, p)
+		if err1 != nil {
+			return
+		}
+	}
 }
 
+//FormatQuery fixes any problems with the query for Magna
 func FormatQuery(queryInput string) {
-
+	//Cursory look at the query to strip out any text that might cause trouble with magna's implementation.
 }
 
 func sendQuery(formatedQuery string) {
+	req, _ := http.NewRequest("POST", "http://localhost:8080/query", nil)
+	// change body to formatedQuery to be sent to magna
+	handleRequestResponse(req)
+}
+
+func handleRequestResponse(req *http.Request) {
 
 }
